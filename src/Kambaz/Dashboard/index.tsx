768f834842754dccdf3daa3455b4e111d7d76e39
Sarang Ajay Patel <sarangp203@gmail.com>
2025-02-05 22:06:11 -0500

    dashboard imgs

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import { Row, Col, Card, Button } from "react-bootstrap";
-import { Link } from "react-router";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link, NavLink } from "react-router";
 
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
-      <h1 id="dashboard-title">Dashboard</h1>
+      <div className="d-flex align-items-center">
+        <NavLink to={"/Kambaz/Courses/1234/Nav"}>
+          <RxHamburgerMenu
+            className="me-4 fs-4 mb-1 text-danger d-md-none d-sm-block"
+            style={{ cursor: "pointer" }}
+          />
+        </NavLink>
+        <h1 id="dashboard-title">Dashboard</h1>
+      </div>
       <hr />
       <h2 id="dashboard-published">Published Courses (7)</h2>
       <hr />
@@ -19,15 +28,21 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\webdev.png"
                   alt="Course 1"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS1234 Web Dev</Card.Title>
+                  <Card.Title className="mt-2 ms-2">CS1234 Web Dev</Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Full stack development
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -40,15 +55,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\ds.jpg"
                   alt="Course 2"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS5678 Data Structures</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS5678 Data Structures
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Understanding algorithms and data structures
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -61,15 +84,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\ai.png"
                   alt="Course 3"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS91011 AI Basics</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS91011 AI Basics
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Introduction to artificial intelligence
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -82,15 +113,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\networking.jpg"
                   alt="Course 4"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS1213 Networking</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS1213 Networking
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Understanding computer networks
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -103,15 +142,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\dbms.jpg"
                   alt="Course 5"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS1415 Database Management</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS1415 Database Management
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Designing and managing databases
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -124,15 +171,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\cloud.jpg"
                   alt="Course 6"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS1617 Cloud Computing</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS1617 Cloud Computing
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Introduction to cloud-based technologies
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
@@ -145,15 +200,23 @@ export default function Dashboard() {
               >
                 <Card.Img
                   variant="top"
-                  src="\images\course.jpg"
+                  src="\images\cybersecurity.jpg"
                   alt="Course 7"
                 />
                 <div>
-                  <Card.Title  className="mt-2 ms-2">CS1819 Cybersecurity</Card.Title>
+                  <Card.Title className="mt-2 ms-2">
+                    CS1819 Cybersecurity
+                  </Card.Title>
                   <Card.Text className="wd-dashboard-course-title ms-2">
                     Foundations of securing digital systems
                   </Card.Text>
-                  <Button variant="primary" className="ms-2 mb-3" style={{width:"100px"}}>Go</Button>
+                  <Button
+                    variant="primary"
+                    className="ms-2 mb-3"
+                    style={{ width: "100px" }}
+                  >
+                    Go
+                  </Button>
                 </div>
               </Link>
             </Card>
