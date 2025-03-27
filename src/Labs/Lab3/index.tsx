962ae35fc5bb9c0f27f7c86b0434e7b0887b277d
Sarang Ajay Patel <sarangp203@gmail.com>
2025-03-27 17:07:31 -0400

    lab4 added

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -29,12 +29,23 @@ import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
+import { useSelector } from "react-redux";
+import { ListGroup } from "react-bootstrap";
 
 export default function Lab3() {
   console.log("hello world!");
+  const { todos } = useSelector((state: any) => state.todosReducer);
   return (
     <div>
       <h2>Lab 3</h2>
+      <ListGroup>
+        {todos.map((todo: any) => (
+          <ListGroup.Item key={todo.id}>
+            {todo.title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <hr />
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
---
+++ src/Labs/Lab4/ChildStateComponent.tsx
@@ -0,0 +1,20 @@
+import { Button } from "react-bootstrap";
+
+export default function ChildStateComponent({
+    counter,
+    setCounter
+   } : {
+    counter: number;
+    setCounter: (counter: number) => void;
+   }) {
+     return (
+       <div id="wd-child-state">
+         <h3>Counter {counter}</h3>
+         <Button variant="success" className="me-3" onClick={() => setCounter(counter + 1)} id="wd-increment-child-state-click">
+           Increment</Button>
+         <Button variant="danger" onClick={() => setCounter(counter - 1)} id="wd-decrement-child-state-click">
+           Decrement</Button>
+         <hr/>
+       </div>
+   );}
+   
