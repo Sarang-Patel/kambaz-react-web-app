696c8500a88f4d0e69fae078fb91252c01857e54
Sarang Ajay Patel <sarangp203@gmail.com>
2025-04-07 17:19:03 -0400

    a6 react first commit

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,49 +1,70 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
-    const {currentUser} = useSelector((state: any) => state.accountReducer);
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
 
-    return (
-        <div id="wd-account-navigation" className="list-group wd fs-5 rounded-0 me-5">
-            {!currentUser && 
-                <NavLink
-                    to={`/Kambaz/Account/Signin`}
-                    className={({ isActive }) =>
-                        `list-group-item border-0 ${isActive ? 'active' : 'text-danger'}`
-                    }
-                    style={({ isActive }) => ({
-                        color: isActive ? 'black' : 'red',
-                    })}
-                >
-                    Sign in
-                </NavLink>}
-                <br />
-                {!currentUser &&<NavLink
-                    to={`/Kambaz/Account/Signup`}
-                    className={({ isActive }) =>
-                        `list-group-item border-0 ${isActive ? 'active' : 'text-danger'}`
-                    }
-                    style={({ isActive }) => ({
-                        color: isActive ? 'black' : 'red',
-                    })}
-                >
-                    Sign up
-                </NavLink>}
-            
-            <br />
-            {currentUser && <NavLink 
-                to={`/Kambaz/Account/Profile`} 
-                className={({ isActive }) => 
-                    `list-group-item border-0 ${isActive ? 'active' : 'text-danger'}`
-                }
-                style={({ isActive }) => ({
-                    color: isActive ? 'black' : 'red',
-                })}
-            >
-                Profile
-            </NavLink>}
-            <br />
-        </div>
-    );
+  return (
+    <div
+      id="wd-account-navigation"
+      className="list-group wd fs-5 rounded-0 me-5"
+    >
+      {!currentUser && (
+        <NavLink
+          to={`/Kambaz/Account/Signin`}
+          className={({ isActive }) =>
+            `list-group-item border-0 ${isActive ? "active" : "text-danger"}`
+          }
+          style={({ isActive }) => ({
+            color: isActive ? "black" : "red",
+          })}
+        >
+          Sign in
+        </NavLink>
+      )}
+      <br />
+      {!currentUser && (
+        <NavLink
+          to={`/Kambaz/Account/Signup`}
+          className={({ isActive }) =>
+            `list-group-item border-0 ${isActive ? "active" : "text-danger"}`
+          }
+          style={({ isActive }) => ({
+            color: isActive ? "black" : "red",
+          })}
+        >
+          Sign up
+        </NavLink>
+      )}
+
+      <br />
+      {currentUser && (
+        <NavLink
+          to={`/Kambaz/Account/Profile`}
+          className={({ isActive }) =>
+            `list-group-item border-0 ${isActive ? "active" : "text-danger"}`
+          }
+          style={({ isActive }) => ({
+            color: isActive ? "black" : "red",
+          })}
+        >
+          Profile
+        </NavLink>
+      )}
+      <br />
+      {currentUser && currentUser.role === "ADMIN" && (
+        <NavLink
+          to={`/Kambaz/Account/Users`}
+          className={({ isActive }) =>
+            `list-group-item border-0 ${isActive ? "active" : "text-danger"}`
+          }
+          style={({ isActive }) => ({
+            color: isActive ? "black" : "red",
+          })}
+        >
+          Users
+        </NavLink>
+      )}
+    </div>
+  );
 }
--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -3,6 +3,23 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(
     `${USERS_API}/signin`,
@@ -51,4 +68,19 @@ export const enrollCourse = async (courseId: string) => {
 
 export const unenrollCourse = async (courseId: string) => {
   return await axiosWithCredentials.delete(`${USERS_API}/current/courses/${courseId}`);
+};
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
+
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
 };
