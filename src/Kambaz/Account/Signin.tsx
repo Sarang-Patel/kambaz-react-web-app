2306a697884ee03fa54fb2528b61960e01ada795
Sarang Ajay Patel <sarangp203@gmail.com>
2025-04-20 17:40:34 -0400

    added team details

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -18,34 +18,48 @@ export default function Signin() {
 
   return (
     <div id="wd-signin-screen">
-      <h3>Sign in</h3>
-      <input
-        defaultValue={credentials.username}
-        placeholder="username"
-        className="wd-username form-control mb-2"
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
-      />
-      <input
-        type="password"
-        defaultValue={credentials.password}
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
-        placeholder="password"
-        className="ws-password form-control mb-2"
-      />
-      <Button
-        id="ws-signin-btn"
-        onClick={signin}
-        className="btn btn-primary w-100 mb-2"
-      >
-        Sign in
-      </Button>
-      <Link to="/kambaz/Account/Signup" id="ws-signup-link">
-        Sign up
-      </Link>
+      <div>
+        <h3>Sign in</h3>
+        <input
+          defaultValue={credentials.username}
+          placeholder="username"
+          className="wd-username form-control mb-2"
+          onChange={(e) =>
+            setCredentials({ ...credentials, username: e.target.value })
+          }
+        />
+        <input
+          type="password"
+          defaultValue={credentials.password}
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+          placeholder="password"
+          className="ws-password form-control mb-2"
+        />
+        <Button
+          id="ws-signin-btn"
+          onClick={signin}
+          className="btn btn-primary w-100 mb-2"
+        >
+          Sign in
+        </Button>
+        <Link to="/kambaz/Account/Signup" id="ws-signup-link">
+          Sign up
+        </Link>
+      </div>
+          <br /><br /><br /><br />
+
+      <div className="team-details bg-warning p-5">
+        <h1>Project - Quiz Section</h1>
+        <h3>Sarang Patel - Grad</h3><br />
+        <a href="https://github.com/Sarang-Patel/kambaz-react-web-app/tree/finalProject">
+          <h5>React Github link</h5>
+        </a>
+        <a href="https://github.com/Sarang-Patel/kambaz-node-server-app/tree/finalProject">
+          <h5>Node Github link</h5>
+        </a>
+      </div>
     </div>
   );
 }
